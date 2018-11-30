5f6f45e590540c0bd3729a7ea8ee05b37207323d
Shreejit <shreejit955@gmail.com>
2018-11-30 17:54:57 +0545

    Updates

---
+++ precache-manifest.ba1cfd11ff1d03c0352b71d4058c4680.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbc1f5e0ba722ecf6e33",
+    "url": "/redux-hackerNews/static/js/runtime~main.bbc1f5e0.js"
+  },
+  {
+    "revision": "078401711edb4110194b",
+    "url": "/redux-hackerNews/static/js/main.07840171.chunk.js"
+  },
+  {
+    "revision": "614309f001222353ac1a",
+    "url": "/redux-hackerNews/static/js/1.614309f0.chunk.js"
+  },
+  {
+    "revision": "078401711edb4110194b",
+    "url": "/redux-hackerNews/static/css/main.c8d54ac3.chunk.css"
+  },
+  {
+    "revision": "cd5cceccd29f98776a4111858ff1f1a3",
+    "url": "/redux-hackerNews/index.html"
+  }
+];
